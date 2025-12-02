a78ca30ec0cb9afc405cca1688d59403cc736647
Sh0dq1004 <atlantis.muro1004@gamil.com>
2025-12-03 01:30:56 +0900

    Changed embedding.py

--- pr_comp_tools/embedding.py
+++ pr_comp_tools/embedding.py
@@ -5,34 +5,18 @@ def load_model():
     device = "cuda" if torch.cuda.is_available() else "cpu"
     return SentenceTransformer('model',device = device)
 
-
 def calc_simil(genSentenceLst, goalSentenceLst):
-    if len(genSentenceLst) != len(goalSentenceLst):
-        print("Length Failure")
-        return
     embed_model=load_model()
-    maxSimil=[0.0, 0]
-    minSimil=[0.0, 0]
-    totalSimil=0.0
+    datas=[]
     for i in range(len(genSentenceLst)):
         emb1 = embed_model.encode(genSentenceLst[i], convert_to_tensor=True)
         emb2 = embed_model.encode(goalSentenceLst[i], convert_to_tensor=True)
         simil = util.cos_sim(emb1, emb2).item()
-        if simil > maxSimil[0]:
-            maxSimil[0]=simil
-            maxSimil[1]=i
-        if simil < minSimil[0]:
-            minSimil[0]=simil
-            minSimil[1]=i
-        totalSimil+=simil
-    print(f"AVE SIMILARITY : {totalSimil/len(genSentenceLst)}")
-    print(f"MAX SIMILARITY : {maxSimil[0]}")
-    print(f"MAX SIMILARITY GENERATED SENTENCE : {genSentenceLst[maxSimil[1]]}")
-    print(f"MAX SIMILARITY TARGET SENTENCE : {goalSentenceLst[maxSimil[1]]}")
-    print(f"MIN SIMILARITY GENERATED SENTENCE : {genSentenceLst[minSimil[1]]}")
-    print(f"MIN SIMILARITY TARGET SENTENCE : {goalSentenceLst[minSimil[1]]}")        
+        datas.append({"similarity": similarity, "generated_pr": generated_pr, "correct_pr": correct_pr})
+    return datas
 
 if __name__ == "__main__":
+
     genSentenceLst = ["これはラーメンです", "これは醤油ラーメンです", "これはおいしい味噌ラーメンです"]
     goalSentenceLst = ["これは味噌ラーメンです","これは味噌ラーメンです","これは味噌ラーメンです"]
     calc_simil(genSentenceLst, goalSentenceLst)
